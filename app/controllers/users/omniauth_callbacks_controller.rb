5dcd9f073c674f62b8f88349d09c4b06e1c462b1
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2013-07-04 14:27:37 +0000

    получение друзей

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,22 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
     @user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+
+
+    # puts "--------------------------------------------------------------------------"
+    # # puts request.env["omniauth.auth"]
+     
+    #  puts request.env["omniauth.auth"]["uid"] 
+    #  puts request.env["omniauth.auth"]["credentials"]["token"]
+
+    # puts "----------------------------------------------------------------------------"
+
+    # TODO  Получаем друзей пользователя и через параметры передаем их в вид.     
+
+
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in_and_redirect @user, :event => :authentication, :params => ["asdasds"]
     else
       flash[:notice] = "authentication error"
       redirect_to root_path
